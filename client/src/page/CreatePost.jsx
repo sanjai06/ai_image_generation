c143d67e4627f58479cf984024cebb02c1492e3e
sanjai06 <nsksanjai@gmail.com>
2023-05-11 06:40:36 +0530

    page update

--- client/src/page/CreatePost.jsx
+++ client/src/page/CreatePost.jsx
@@ -31,7 +31,7 @@ const CreatePost = () => {
         const response = await fetch('http://localhost:8080/api/v1/dalle', {
           method: 'POST',
           headers: {
-            'Content-Type': 'application/json',
+            'Content-Type':'application/json',
           },
           body: JSON.stringify({
             prompt: form.prompt,
